0fdac90c1735eec4f4d26157566821358b7ccf05
kamal <kamalpandi123@gmail.com>
2026-09-15 12:31:34 +0530

    added setups

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import json
+import os
 
+if __name__ == "__main__":
+    folder_path = "src/task_tracker/json_data/"
+    file_path = os.path.join(folder_path, "json_data.json")
+    from src.task_tracker import main
+
+    if not os.path.isfile(file_path):
+        initial_data = {}
+        with open(file_path, "w") as file:
+            json.dump(initial_data, file)
+
+    main()
